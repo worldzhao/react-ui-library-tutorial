26452ceaa17c7eb2f501e6df7a61c7aa9d5a2c51
zhaozhiwen <zhiwen.zhao@ximalaya.com>
2019-12-25 13:16:23 +0800

    feat(build): 生成css.js文件

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const babel = require('gulp-babel');
 const less = require('gulp-less');
 const autoprefixer = require('gulp-autoprefixer');
 const cssnano = require('gulp-cssnano');
+const through2 = require('through2');
 
 const paths = {
   dest: {
@@ -15,9 +16,22 @@ const paths = {
 };
 
 /**
+ * 当前组件样式 import './index.less' => import './index.css'
+ * 依赖的其他组件样式 import '../test-comp/style' => import '../test-comp/style/css.js'
+ * 依赖的其他组件样式 import '../test-comp/style/index.js' => import '../test-comp/style/css.js'
+ * @param {string} content
+ */
+function cssInjection(content) {
+  return content
+    .replace(/\/style\/?'/g, "/style/css'")
+    .replace(/\/style\/?"/g, '/style/css"')
+    .replace(/\.less/g, '.css');
+}
+
+/**
  * 编译脚本文件
- * @param {*} babelEnv babel环境变量
- * @param {*} destDir 目标目录
+ * @param {string} babelEnv babel环境变量
+ * @param {string} destDir 目标目录
  */
 function compileScripts(babelEnv, destDir) {
   const { scripts } = paths;
@@ -25,6 +39,21 @@ function compileScripts(babelEnv, destDir) {
   return gulp
     .src(scripts)
     .pipe(babel()) // 使用gulp-babel处理
+    .pipe(
+      through2.obj(function z(file, encoding, next) {
+        this.push(file.clone());
+        // 找到目标
+        if (file.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
+          const content = file.contents.toString(encoding);
+          file.contents = Buffer.from(cssInjection(content)); // 处理文件内容
+          file.path = file.path.replace(/index\.js/, 'css.js'); // 文件重命名
+          this.push(file); // 新增该文件
+          next();
+        } else {
+          next();
+        }
+      }),
+    )
     .pipe(gulp.dest(destDir));
 }
 
