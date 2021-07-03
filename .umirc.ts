e2f80bc36a66c12e7854e849dee522662238089b
zhiwen.zhao <zhiwen.zhao@ximalaya.com>
2021-07-03 15:58:19 +0800

    feat: 使用 github pages 部署文档站点

--- .umirc.ts
+++ .umirc.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'dumi';
 
+// 此处更换为自己的仓库名
+let base = '/react-ui-library-tutorial';
+let publicPath = '/react-ui-library-tutorial/';
+
+if (process.env.DOC_BUILD_ENV === 'local') {
+  base = undefined;
+  publicPath = undefined;
+}
+
 export default defineConfig({
   title: 'Happy UI',
   mode: 'site',
   outputPath: 'doc-site',
   exportStatic: {},
   dynamicImport: {},
+  base,
+  publicPath,
 });
