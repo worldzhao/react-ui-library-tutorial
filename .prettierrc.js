9e20b5c304e1250694521fa342ea2c39f32cf5e8
worldzhao <1007326924@qq.com>
2022-11-26 20:00:51 +0800

    chore: update lint tools version

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,8 @@ const fabric = require('@umijs/fabric');
 
 module.exports = {
   ...fabric.prettier,
+  plugins: [
+    require.resolve('prettier-plugin-packagejson'),
+    require.resolve('prettier-plugin-organize-imports'),
+  ],
 };
