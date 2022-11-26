a2ace101839e1218f9bbd09fee448949f6c70a3c
worldzhao <1007326924@qq.com>
2022-11-26 22:49:16 +0800

    chore: update dumi version and lint tools version

---
+++ .dumirc.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'dumi';
+import path from 'path';
+
+let base: string | undefined;
+let publicPath: string | undefined;
+
+// Github Pages 部署时需要更换为自己的仓库名
+if (process.env.NODE_ENV === 'production' && process.env.PREVIEW !== '1') {
+  base = '/react-ui-library-tutorial';
+  publicPath = '/react-ui-library-tutorial/';
+}
+
+export default defineConfig({
+  base,
+  publicPath,
+  title: 'Happy UI',
+  outputPath: 'doc-site',
+  resolve: {
+    docDirs: ['docs'],
+    atomDirs: [{ type: 'component', dir: 'src' }],
+  },
+  exportStatic: {},
+  forkTSChecker: {},
+  extraBabelPlugins: [
+    [
+      'import',
+      {
+        libraryName: '@zzzzw/happy-ui',
+        libraryDirectory: '',
+        customStyleName: (name) => path.resolve(__dirname, `src/${name}/style/index.ts`),
+      },
+    ],
+  ],
+});
